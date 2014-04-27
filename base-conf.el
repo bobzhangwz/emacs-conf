c8ec793473861ae1016cbfb164ea5f5026c51e44
poe <zhpooer@gmail.com>
2014-04-27 12:15:12 +0800

    todo

--- base-conf.el
+++ base-conf.el
@@ -69,7 +69,7 @@
               (equal (string-match " \\*.*\\ *" (buffer-name (current-buffer))) 0))
     (linum-mode 1)))
 (define-globalized-minor-mode global/linum-mode linum-mode linum/on)
-(global/linum-mode 1)
+;; (global/linum-mode 1)
 
 
 ;;; whitespace
@@ -489,3 +489,18 @@
 (require 'todotxt)
 (setq todotxt-file "~/.todo")
 (global-set-key (kbd "C-x t") 'todotxt)
+
+
+;; backup
+
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 30              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+      )
